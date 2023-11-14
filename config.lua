63695b65258e1ffa3030a34a6bd36292bf87f0bb
Chowewow <chowewoow@gmail.com>
2023-11-14 14:43:15 +0800

    first commit

---
+++ config.lua
@@ -0,0 +1,89 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+
+lvim.builtin.nvimtree.active = false
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+lvim.plugins = {
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    opts = {
+      -- table: default groups
+      groups = {
+        "Normal",
+        "NormalNC",
+        "Comment",
+        "Constant",
+        "Special",
+        "Identifier",
+        "Statement",
+        "PreProc",
+        "Type",
+        "Underlined",
+        "Todo",
+        "String",
+        "Function",
+        "Conditional",
+        "Repeat",
+        "Operator",
+        "Structure",
+        "LineNr",
+        "NonText",
+        "SignColumn",
+        "CursorLineNr",
+        "EndOfBuffer",
+        "FloatBorder",
+      },
+      -- table: additional groups that should be cleared
+      extra_groups = {
+        "NormalFloat",
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+        "NeoTreeNormalFloat",
+        "NeoTreeEndOfBuffer",
+      },
+      -- table: groups you don't want to clear
+      exclude_groups = {},
+    }
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function ()
+      require('neo-tree').setup {
+        window = {
+          position = "right",
+          width = 25,
+          mapping_options = {
+            noremap = true,
+            nowait = true,
+          },
+        },
+      }
+    end,
+  },
+}
+
+lvim.autocommands = {
+
+}
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+
